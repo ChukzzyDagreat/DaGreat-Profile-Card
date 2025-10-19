e8f7b6976eaca6c0fa817ce79bba4a6bee0157f2
Ochuko Akporue <ochukoakporue@gmail.com>
2025-10-19 02:38:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+ Profile Card Website
+
+A simple and responsive **profile card web page** built using **HTML**, **CSS**, and **JavaScript**.  
+It showcases a user’s photo, name, job title, hobbies,dislikes and contact links in a clean, accessible layout.
+
+---
+
+## Description
+
+This project demonstrates a small, modern **frontend component** — a personal profile card that can be reused in a portfolio or website.  
+The design follows **semantic HTML** principles, supports **keyboard navigation**, and is fully **responsive** for both mobile and desktop screens.
+
+---
+
+## ✨ Features
+
+- Responsive layout using modern CSS  
+- Fully accessible (keyboard & screen reader friendly)  
+- Displays profile image, name, role, hobbies, dislikes and social links  
+- Hover animations for interactivity (cover photo) 
+- Clean, minimal UI design  
+
+---
+
+## Tech Stack
+
+- **HTML5** — structure and semantics  
+- **CSS3** — layout and design  
+- **JavaScript (Vanilla)** — interactions and behaviour  
+
+
+---
+
+ Author
+## Ochuko Great Akporue
+
---
+++ script.js
@@ -0,0 +1,52 @@
+// Example: Simple interactivity with vanilla JavaScript
+/*const profileCard = document.querySelector('[data-test="profile-card"]');
+profileCard.addEventListener("click", () => {
+  alert("You clicked on the profile card!");
+});*/
+
+// Navigation functionality for social links
+const btn = document.querySelector('.custom_btn');
+
+btn.addEventListener('click', () => {
+  console.log('Clicked with mouse');
+});
+
+btn.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' || e.key === ' ') {
+    console.log('Activated with keyboard');
+    // do the same action as click
+    e.preventDefault();
+  }
+});
+
+
+// Profile card section navigation functionality
+const buttons = document.querySelectorAll('.card_buttons button');
+const sections = document.querySelectorAll('.card_section');
+const card = document.querySelector('.card');
+
+const handleButtonClick = (e) => {
+  const targetSection = e.target.getAttribute('data-section');
+  const section = document.querySelector(targetSection);
+  
+//  use the ternary condition to check the condition
+
+  targetSection !== "#about"
+  ? card.classList.add('is_active')
+  : card.classList.remove('is_active');
+
+  card.setAttribute('data-state', targetSection);
+  //  remove the active section
+  sections.forEach((s) => s.classList.remove('is_active'));
+  // remove the active button
+  buttons.forEach((b) => b.classList.remove('is_active'));
+
+  // add the target section to button as active on click
+  e.target.classList.add('is_active');
+  section.classList.add('is_active');
+};
+
+// add listener event for all the buttons
+buttons.forEach((btn) => {
+  btn.addEventListener('click', handleButtonClick);
+});
